bae04ed3a2f0779ff94c08976233d95f998a6661
ItzPatel <nisargp2810@gmail.com>
2025-07-29 14:37:37 +0530

    Add files via upload

---
+++ FBS/Core Python/Demo Projects/demo3.py
@@ -0,0 +1,8 @@
+i = 2 + 5j
+print(type(i))
+
+str = "first bit solutions"
+print(type(str))
+
+list = [1,2,3,4]
+print(type(list))
---
+++ FBS/Core Python/Demo Projects/operators.py
@@ -0,0 +1,25 @@
+#1. Arithmatic Operators (+, -, *, /, %, //, **)
+a = 5
+b = 20
+#addition
+c = a + b
+#subtraction
+d = a - b
+#multiplication
+e = a * b
+#division
+f = a / b
+#modulus
+g = a % b
+#floor division
+h = a // b
+#exponentiation
+i = a ** b
+#Printing results
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
